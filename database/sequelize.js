005c78dcab242b7deb5c2a90d0702cb135af3fb5
Gustavo Cruz <82175584+Gustavo-Cruzz@users.noreply.github.com>
2025-04-30 10:53:09 -0300

    Update sequelize.js for vercel connection

--- database/sequelize.js
+++ database/sequelize.js
@@ -2,12 +2,12 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.POSTGRES_DATABASE, 
+  process.env.POSTGRES_USER,     
+  process.env.POSTGRES_PASSWORD, 
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: process.env.POSTGRES_HOST,
+    port: 5432,                     
     dialect: 'postgres',
     logging: false
   }
