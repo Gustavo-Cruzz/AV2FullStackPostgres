f310317d52beb4c1e9e05548457a342c40c8786a
Gustavo Cruz <82175584+Gustavo-Cruzz@users.noreply.github.com>
2025-05-01 17:26:42 -0300

    Minor adjustments to run in postgres

--- services/authService.js
+++ services/authService.js
@@ -3,26 +3,29 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const register = async (name, email, password) => {
-  if (!password) {
-    throw new Error('A senha não pode estar vazia.');
-  }
+    
+  const existingUser = await User.findOne({
+    where: {
+      email: email 
+    }
+  });
 
-  if (!email || !email.includes('@')) {
-    throw new Error('Por favor, insira um e-mail válido.');
-  }
-  
-  const existingUser = await User.findOne({ email });
   if (existingUser) throw new Error('E-mail já registrado');
 
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = new User({ name, email, password: hashedPassword });
-
   await newUser.save();
+
   return newUser;
 };
 
 const login = async (email, password) => {
-  const user = await User.findOne({ where: { email } });
+  const user = await User.findOne({
+    where: {
+      email: email 
+    }
+  });
+
   if (!user) throw new Error('Usuário não encontrado');
 
   const isMatch = await bcrypt.compare(password, user.password);
